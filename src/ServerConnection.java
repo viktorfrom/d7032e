775c34fb0ae0df2af7c0e6b964274b0f5c7a9af9
Viktor From <viktor.from91@gmail.com>
2019-10-24 08:34:12 +0200

    Add ServerConnection, WinCondition

---
+++ src/ServerConnection.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ServerConnection {
+    private ArrayList<Monster> monsters;
+
+    public ServerConnection(ArrayList<Monster> monsters) {
+        this.monsters = monsters;
+    }
+
+    public void connectToClient() throws IOException {
+        try {
+            ServerSocket aSocket = new ServerSocket(2048);
+            // assume two online clients
+            for (int onlineClient = 0; onlineClient < 2; onlineClient++) {
+                Socket connectionSocket = aSocket.accept();
+                BufferedReader inFromClient = new BufferedReader(
+                        new InputStreamReader(connectionSocket.getInputStream()));
+                DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+                outToClient.writeBytes("You are the monster: " + this.monsters.get(onlineClient).name + "\n");
+
+                this.monsters.get(onlineClient).connection = connectionSocket;
+                this.monsters.get(onlineClient).inFromClient = inFromClient;
+                this.monsters.get(onlineClient).outToClient = outToClient;
+
+                System.out.println("Connected to " + this.monsters.get(onlineClient).name);
+            }
+        } catch (IOException Error) {
+            System.out.println("Error: Could not establish connection to client(s)." + Error);
+
+        }
+    }
+}
---
+++ src/WinCondition.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+
+public class WinCondition {
+    private ArrayList<Monster> monsters;
+    private SendMessage sendMessage;
+    private int alive;
+    private String aliveMonster;
+
+    public WinCondition(ArrayList<Monster> monsters, SendMessage sendMessage) {
+        this.monsters = monsters;
+        this.sendMessage = sendMessage;
+        this.alive = 0;
+        this.aliveMonster = "";
+    }
+
+    public void winCondition() {
+        if (monstersAlive() || numStars()) {
+            System.exit(0);
+        }
+    }
+
+    private boolean numStars() {
+        if (alive == 1) {
+            for (int victory = 0; victory < this.monsters.size(); victory++) {
+                this.sendMessage.sendMessage(victory,
+                        "Victory: " + this.aliveMonster + " has won by being the only one alive\n");
+            }
+            return true;
+        }
+        return false;
+    }
+
+    private boolean monstersAlive() {
+        for (int mon = 0; mon < this.monsters.size(); mon++) {
+            if (this.monsters.get(mon).stars >= 5) {
+                for (int victory = 0; victory < this.monsters.size(); victory++) {
+                    this.sendMessage.sendMessage(victory, "Victory: " + monsters.get(mon).name + " has won by stars\n");
+                }
+                return true;
+            }
+            if (this.monsters.get(mon).currentHealth > 0) {
+                this.alive++;
+                this.aliveMonster = this.monsters.get(mon).name;
+            }
+        }
+        return false;
+    }
+}
