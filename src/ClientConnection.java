e2dc6b88ff41a92f6596fdcd13be1d1e237cafcc
Viktor From <viktor.from91@gmail.com>
2019-10-26 16:38:34 +0200

    Add ClientConnection

---
+++ src/ClientConnection.java
@@ -0,0 +1,67 @@
+import java.util.*;
+import java.io.*;
+import java.util.Scanner;
+
+public class ClientConnection {
+    private boolean bot;
+    private Scanner scanner = new Scanner(System.in);
+
+    public ClientConnection(boolean bot) throws IOException {
+        this.bot = bot;
+    }
+
+    public void serverCommunication(Random rnd, DataOutputStream outToServer, BufferedReader inFromServer)
+            throws IOException {
+        while (true) {
+            String[] message = inFromServer.readLine().split(":");
+            for (int i = 0; i < message.length; i++) {
+                System.out.println(message[i].toString());
+            }
+            if (message[0].equalsIgnoreCase("VICTORY")) {
+                outToServer.writeBytes("Bye!\n");
+            } else if (message[0].equalsIgnoreCase("ATTACKED")) {
+                attackedClient(outToServer);
+            } else if (message[0].equalsIgnoreCase("ROLLED")) {
+                rerollClient(outToServer);
+            } else if (message[0].equalsIgnoreCase("PURCHASE")) {
+                purchaseClient(outToServer);
+            } else {
+                noChoiceClient(outToServer);
+            }
+            System.out.println("\n");
+        }
+    }
+
+    private void attackedClient(DataOutputStream outToServer) throws IOException {
+        if (bot)
+            outToServer.writeBytes("YES\n");
+        else {
+            outToServer.writeBytes(scanner.nextLine() + "\n");
+        }
+    }
+
+    private void rerollClient(DataOutputStream outToServer) throws IOException {
+        if (bot)
+            outToServer.writeBytes("YES\n");
+        else {
+            outToServer.writeBytes(scanner.nextLine() + "\n");
+        }
+    }
+
+    private void purchaseClient(DataOutputStream outToServer) throws IOException {
+        if (bot)
+            outToServer.writeBytes("-1\n");
+        else
+            outToServer.writeBytes(scanner.nextLine() + "\n");
+    }
+
+    private void noChoiceClient(DataOutputStream outToServer) throws IOException {
+        if (bot)
+            outToServer.writeBytes("OK\n");
+        else {
+            System.out.println("Press [ENTER]");
+            scanner.nextLine();
+            outToServer.writeBytes("OK\n");
+        }
+    }
+}
